a6b36267fbaeaef0e45d0a4ec9c902250500e24f
Alexander Bliss <alexanderbliss@Alexanders-MacBook-Pro.local>
2017-10-04 08:25:31 -0500

    completed task

--- script.js
+++ script.js
@@ -2,10 +2,27 @@ var locations = []; // this array will hold your objects
 
 // object constructor
 
+var State = function (state,revenue,locationCount){
+  this.state = state;
+  this.revenue = revenue;
+  this.locationCount = locationCount;
+
+
+  this.averageRevenue = function() {
+    return Math.round(this.revenue/this.locationCount);
+  };
+};
+
 // object instances
+ var location0 = new State ('IL', 5000, 12);
+ var location1 = new State ('MN', 300 , 3);
+ var location2 = new State ('NV', 25000,1);
+
 
 // push object instances to locations array
+locations.push(location0, location1, location2);
 
+console.log(locations);
 // DO NOT MODIFY THIS CODE
 $(document).ready(function () {
   locations.forEach(function (element, index) {
